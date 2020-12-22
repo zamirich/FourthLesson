585aed77411504f1f45c7beb01cf04b28ab2482d
ZamiRich <argashokovzamir@gmail.com>
2020-12-22 22:42:32 +0100

    Initial Commit

---
+++ FourthLesson/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FourthLesson
+//
+//  Created by ZamiRich on 22.12.2020.
+//  Copyright © 2020 ZamiRich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
